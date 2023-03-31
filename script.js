b6723f0f34ebc3656279c2389660effd0dd3bc38
Ayanokoujifl <guleite3@gmail.com>
2023-03-31 11:53:21 -0300

    initial configure for navbar

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
-window.onload = async () => {
-  const ul = document.querySelector("ul");
+const ul = document.querySelector("ul");
+const searched = document.querySelector("#search");
 
-  await fetch(
-    "https://api.nasa.gov/planetary/apod?api_key=6gYWbMRm24fo2nVDui7SbjcSDPEljqcTlSlxDe2f&count=10&hd=true&thumbs=true"
-  )
+const search = async () => {
+  console.log(searched.value);
+  await fetch(`https://images-api.nasa.gov/search?q=${searched.value}`)
     .then((response) => response.json())
     .then((data) => {
       console.log(data);
