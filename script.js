f061ab3830bd51854bd79576e10189027c6ff174
Ayanokoujifl <guleite3@gmail.com>
2023-03-31 15:55:14 -0300

    search function added

--- script.js
+++ script.js
@@ -1,11 +1,30 @@
 const ul = document.querySelector("ul");
 const searched = document.querySelector("#search");
-
+const main = document.querySelector("main");
 const search = async () => {
+  main.innerHTML = "";
   console.log(searched.value);
-  await fetch(`https://images-api.nasa.gov/search?q=${searched.value}`)
+  await fetch(
+    `https://images-api.nasa.gov/search?q=${searched.value}&media_type=image`
+  )
     .then((response) => response.json())
     .then((data) => {
-      console.log(data);
+      const items = [];
+      items.push(data.collection.items);
+      const item = items[0];
+      item.map((item) => {
+        console.log(item);
+        const links = item.links;
+        links.map((link) => {
+          const div = document.createElement("div");
+          div.classList.add("image-wrapper");
+          let title = item.data[0].title;
+          div.innerHTML = `
+          <h4 class="image-title">${title}</h4>
+          <img src=${link.href} alt="" class="image" />
+          `;
+          main.appendChild(div);
+        });
+      });
     });
 };
